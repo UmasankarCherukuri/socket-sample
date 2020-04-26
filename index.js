58bb91bf644ffbb6657d68bb4dc5505928c30ae0
unknown <umasankarcherukuri@gmail.com>
2020-04-26 10:24:43 +0530

    deployed to heroku

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-var app = require("express")();
-var http = require("http").createServer(app);
-var io = require("socket.io")(http);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
